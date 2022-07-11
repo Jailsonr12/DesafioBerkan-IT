2e4d5e2d9717ed578df0668a1d31f70a3c5f957a
Jailsonr12 <jailsonroth13@gmail.com>
2022-07-11 18:02:56 -0300

    Programa

---
+++ desafio/CalculoImpostoDeRendaMensal.java
@@ -0,0 +1,77 @@
+package desafio;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class CalculoImpostoDeRendaMensal {
+
+	public static void main(String[] args) {
+
+		// Variavel
+
+		double salario = 0, imposto_inss = 0, salarioDesconto = 0, imposto_irpf = 0, dependente_desconto = 0;
+		int numero_depedente = 0;
+
+		// Entrada
+		Locale.setDefault(Locale.US);
+		Scanner teclado = new Scanner(System.in);
+		System.out.println("------------------------------------");
+		System.out.println(" Calculo do Imposto de Renda Mensal");
+		System.out.println("------------------------------------");
+		System.out.print("Informe o salario:");
+		salario = teclado.nextDouble();
+		while (salario <= 0) {
+			System.out.println("Imposto impossivel de calcular \nSalario deve ser maior que 0 (zero)");
+			System.out.println("------------------------------------");
+			System.out.print("Informe o salario:");
+			salario = teclado.nextDouble();
+		}
+		System.out.print("Informe quantidade de depedente:");
+		numero_depedente = teclado.nextInt();
+		System.out.println("------------------------------------");
+
+		// Processamento
+
+		if (salario <= 0) {
+			System.out.println("Imposto impossivel de calcular \nSalario deve ser maior que 0 (zero)");
+		}
+		if (salario <= 1751.81) {
+			imposto_inss = salario * 0.08;
+		} else if (salario <= 2919.372) {
+			imposto_inss = salario * 0.09;
+		} else if (salario <= 5839.45) {
+			imposto_inss = salario * 0.11;
+		} else if (salario > 5839.45) {
+			imposto_inss = 642.34;
+		}
+
+		dependente_desconto = numero_depedente * 189.59;
+		salarioDesconto = salario - dependente_desconto;
+
+		if (salarioDesconto <= 1903.98) {
+			imposto_irpf = 0;
+		} else if ((salario - dependente_desconto) <= 2826.65) {
+			imposto_irpf = (salarioDesconto * 0.075 - 142.80);
+
+		} else if (salarioDesconto <= 3751.05) {
+			imposto_irpf = (salarioDesconto * 0.15 - 354.80);
+
+		} else if (salarioDesconto <= 4664.68) {
+			imposto_irpf = (salarioDesconto * 0.225 - 636.13);
+
+		} else if (salarioDesconto > 4664.68) {
+			imposto_irpf = (salarioDesconto * 0.275 - 869.36);
+		}
+
+		// Saida
+
+		System.out.println("CALCULOS DOS IMPOSTOS \n");
+		System.out.printf("Desconto do imposto INSS: R$%.2f \n", imposto_inss);
+		System.out.printf("Desconto do imposto IRPF: R$%.2f \n", imposto_irpf);
+		System.out.printf("Desconto tota: R$%.2f \n", (imposto_irpf + imposto_inss));
+		System.out.printf("Salario final: R$%.2f \n ", salario - (imposto_irpf + imposto_inss));
+
+		teclado.close();
+	}
+
+}
